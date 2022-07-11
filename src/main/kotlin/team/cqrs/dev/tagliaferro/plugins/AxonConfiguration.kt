7738e671948c36d48d94c8e6cb25dd16672c8fe2
Adriano <adrianotagliaferro@hotmail.com>
2022-07-11 14:56:59 -0300

    chore: change axon command gateway config and log level

--- src/main/kotlin/team/cqrs/dev/tagliaferro/plugins/AxonConfiguration.kt
+++ src/main/kotlin/team/cqrs/dev/tagliaferro/plugins/AxonConfiguration.kt
@@ -1,25 +1,22 @@
 package team.cqrs.dev.tagliaferro.plugins
 
 import org.axonframework.commandhandling.SimpleCommandBus
-import org.axonframework.commandhandling.gateway.DefaultCommandGateway
+import org.axonframework.commandhandling.gateway.CommandGateway
 import org.axonframework.config.DefaultConfigurer
 import org.axonframework.eventsourcing.eventstore.inmemory.InMemoryEventStorageEngine
 import team.cqrs.dev.tagliaferro.handlers.PlayerCommandHandler
 
-fun commandGatewayBuilder(): DefaultCommandGateway {
+fun commandGatewayBuilder(): CommandGateway {
     val commandBus = SimpleCommandBus
         .builder()
         .build()
 
-    DefaultConfigurer
+    val configurer = DefaultConfigurer
         .defaultConfiguration()
         .configureAggregate(PlayerCommandHandler::class.java)
         .configureEmbeddedEventStore { InMemoryEventStorageEngine() }
         .configureCommandBus { commandBus }
         .start()
 
-    return DefaultCommandGateway
-        .builder()
-        .commandBus(commandBus)
-        .build()
+    return configurer.commandGateway()
 }
