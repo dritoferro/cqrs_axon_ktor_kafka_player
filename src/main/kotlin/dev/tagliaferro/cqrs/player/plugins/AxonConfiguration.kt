8ca14eb96890d51070a717103eeac9a3f93c3d1d
Adriano <adrianotagliaferro@hotmail.com>
2022-09-12 15:46:21 -0300

    feat: add java time module

--- src/main/kotlin/dev/tagliaferro/cqrs/player/plugins/AxonConfiguration.kt
+++ src/main/kotlin/dev/tagliaferro/cqrs/player/plugins/AxonConfiguration.kt
@@ -1,6 +1,7 @@
 package dev.tagliaferro.cqrs.player.plugins
 
 import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import com.fasterxml.jackson.module.kotlin.registerKotlinModule
 import com.thoughtworks.xstream.XStream
 import dev.tagliaferro.cqrs.player.handlers.PlayerCommandHandler
@@ -86,7 +87,10 @@ object AxonConfiguration {
         val basePackage = "dev.tagliaferro.cqrs.player.**"
 
         val om = ObjectMapper()
-        om.registerKotlinModule()
+        om
+            .registerKotlinModule()
+            .registerModule(JavaTimeModule())
+
 
         val eventSerializer = JacksonSerializer
             .builder()
